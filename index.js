43a3016631badb85298a8aae1b6aaa55bca6a6d4
Shane K. Panter <shane@foundationcode.com>
2025-12-16 18:02:50 -0700

    Implement drawTriangle function skeleton
    Added drawTriangle function and initial test call

---
+++ index.js
@@ -0,0 +1,10 @@
+function drawTriangle(triangleSize) {
+
+   // TODO: Add your solution here
+   
+}
+
+console.log("Testing drawTriangle()...");
+
+// TODO: Test drawTriangle() with different arguments
+drawTriangle(4);
