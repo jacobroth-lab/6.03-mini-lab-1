c7a9424b971d284cea97e8f62771fe85a88ee89f
Jacob Roth <jacobroth@u.boisestate.edu>
2026-02-19 13:14:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function drawTriangle(triangleSize) {
-
+    for (let i = 1; i <= triangleSize; i++){
+        let row = "";
+        for (let j = 1; j <= i; j++){
+            row += "*";
+        }
+        console.log(row);
+    }
 }
 
 export default drawTriangle;
+
